3e594b6f1f3056a8e4ae1a61ad6e5f241996967b
DeepanS27 <131382005+DeepanS27@users.noreply.github.com>
2024-02-09 09:06:55 +0530

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBi4PY5r6uQjU5r025OYwqaeNKECru_SHA",
+  authDomain: "authentication-dee17.firebaseapp.com",
+  databaseURL: "https://authentication-dee17-default-rtdb.firebaseio.com",
+  projectId: "authentication-dee17",
+  storageBucket: "authentication-dee17.appspot.com",
+  messagingSenderId: "895015187256",
+  appId: "1:895015187256:web:48a57fe5b17a67b61b87cd",
+  measurementId: "G-EZLNT0SVCP",
+};
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
